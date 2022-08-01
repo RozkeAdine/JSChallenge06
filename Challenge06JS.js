4ee1aef65a846cc6f00522af5199a302a6a49326
RozkeAdine <105638291+RozkeAdine@users.noreply.github.com>
2022-08-01 14:46:57 +0200

    Update Challenge06JS.js

--- Challenge06JS.js
+++ Challenge06JS.js
@@ -1,14 +1,3 @@
-//tache numero 1
-// const DauphinsScore1 = (44 + 23 + 71) / 3;
-// console.log('moyenne D1 :'+ DauphinsScore1);
-// const KoalaScore1 = (65 + 54 + 49) / 3;
-// console.log('moyenne K1 :' + KoalaScore1);
-//
-// const DauphinsScore2 = (85 + 54 + 41) / 3;
-// console.log('moyenne D2 :' + DauphinsScore2);
-// const KoalaScore2 = (23 + 34 + 27) / 3;
-// console.log('moyenne K2 :' + KoalaScore2);
-
 function calcAverage(score1, score2, score3){
    return ((score1 + score2 + score3) / 3)
 }
@@ -34,14 +23,3 @@ function checkWinner(team1, score1 , team2, score2){
 }
 
 checkWinner('Dauphin', dauphin2, 'Koala', koala2)
-
-//tache numero 2
-// console.log("comparaison et affichage")
-// console.log("premier jeu")
-// if (meansDauphinsScore1 < meansKoalasScore1) {
-//     console.log("Pour ce premier jeu, les gagnants sont les Koalas !")
-// }else if (meansDauphinsScore1 > meansKoalasScore1){
-//     console.log("Pour ce premier jeu, les gagnants sont les Dauphins !")
-// }else if (meansDauphinsScore1 === meansKoalasScore1){
-//     console.log("Pour ce premier jeu, les équipes sont à égalité !")
-// };
