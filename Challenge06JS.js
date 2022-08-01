73e39803e05e0f871f74c798278af2535f17681f
RozkeAdine <rozke-adine.bouyahia-std@donkey.school>
2022-08-01 14:42:46 +0200

    update challenge06

--- Challenge06JS.js
+++ Challenge06JS.js
@@ -1,14 +1,3 @@
-//tache numero 1
-// const DauphinsScore1 = (44 + 23 + 71) / 3;
-// console.log('moyenne D1 :'+ DauphinsScore1);
-// const KoalaScore1 = (65 + 54 + 49) / 3;
-// console.log('moyenne K1 :' + KoalaScore1);
-//
-// const DauphinsScore2 = (85 + 54 + 41) / 3;
-// console.log('moyenne D2 :' + DauphinsScore2);
-// const KoalaScore2 = (23 + 34 + 27) / 3;
-// console.log('moyenne K2 :' + KoalaScore2);
-
 function calcAverage(score1, score2, score3){
    return ((score1 + score2 + score3) / 3)
 }
@@ -33,4 +22,4 @@ function checkWinner(team1, score1 , team2, score2){
    }
 }
 
-checkWinner('Dauphin', dauphin2, 'Koala', koala2)
+checkWinner('Dauphin', dauphin2, 'Koala', koala2);
